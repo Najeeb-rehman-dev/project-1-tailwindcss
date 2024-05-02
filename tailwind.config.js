808892e7c67640b6844e450b5e7b72102e66c675
Najeeb-rehman-dev <mohdnajeeburrehman@gmail.com>
2024-05-02 14:07:56 +0530

    updated the tailwind config with custom fontSizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "12px",
+        base: "14px",
+        md: "16px",
+        lg: "20px",
+        xl: "24px",
+        "2xl": "28px",
+        "3xl": "36",
+      },
+    },
   },
   plugins: [],
 };
